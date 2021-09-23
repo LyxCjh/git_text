ae527d944a64a8020fbf41e5106215efb898f5a2
lyx <lyx@tedu.com>
2021-09-23 15:54:46 +0800

    hot-fix commit1

--- src/main/java/cn/tedu/git/git_demo01.java
+++ src/main/java/cn/tedu/git/git_demo01.java
@@ -4,5 +4,7 @@ public class git_demo01 {
     public static void main(String[] args) {
         System.out.println("hello git1!!!");
         System.out.println("hello git2!!!");
+        System.out.println("==============");
+        System.out.println("hello git===>hot-fix!!!");
     }
 }
