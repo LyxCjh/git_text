120a3d6058ccefbd2b543577fdffa86d677e8882
lyx <lyx@tedu.com>
2021-09-23 15:42:34 +0800

    second commit

--- src/main/java/cn/tedu/git/git_demo01.java
+++ src/main/java/cn/tedu/git/git_demo01.java
@@ -2,6 +2,7 @@ package cn.tedu.git;
 
 public class git_demo01 {
     public static void main(String[] args) {
-        System.out.println("hello git!!!");
+        System.out.println("hello git1!!!");
+        System.out.println("hello git2!!!");
     }
 }
